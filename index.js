6995a3a9ff07e278c46379392e833dde6f73d08a
Yinson Tso <yinson.tso@gmail.com>
2023-03-08 01:05:27 -0500

    Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    let newArr = [...cats, name]
+    return newArr
+}
+
+function prependCat(name){
+    let newArr = [name, ...cats]
+    return newArr
+}
+
+function removeLastCat(){
+    return cats.slice(0,2)
+}
+
+function removeFirstCat(){
+    return cats.slice (1)
+}
